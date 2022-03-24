0081a4a0c7c872901dfa9b50bb112633a843443f
adria <adria@DESKTOP-T2UISSQ>
2022-03-24 21:32:11 +0100

    commit con la clase Cadena terminada

--- cadena/src/cadena/Cadena.java
+++ cadena/src/cadena/Cadena.java
@@ -1,5 +1,37 @@
 package cadena;
 
 public class Cadena {
+
+	public int longitud(String cadena) {
+		int longitud = cadena.length();
+		return longitud;
+	}
+
+	public int vocales(String cadena) {
+		int contador = 0;
+		for (int i = 0; i < cadena.length(); i++) {
+			if (cadena.charAt(i) == 'a' || cadena.charAt(i) == 'e' ||
+				cadena.charAt(i) == 'i' || cadena.charAt(i) == 'o' || 
+				cadena.charAt(i) == 'u') {
+				contador++;
+			}
+		}
+		return contador;
+	}
 	
+	public String invertir (String cadena) {
+		StringBuilder sb = new StringBuilder(cadena);
+		cadena = sb.reverse().toString();
+		return cadena;
+	}
+	
+	public int contarLetra (String cadena, char caracter) {
+		int contador = 0;
+		for (int i = 0; i < cadena.length(); i++) {
+			if (cadena.charAt(i) == caracter) {
+				contador ++;
+			}
+		}
+		return contador;
+	}
 }
