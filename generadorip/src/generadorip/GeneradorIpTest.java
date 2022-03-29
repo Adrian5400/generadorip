d2bb1fde2ffecf8cc57e59b46286f628680483b6
adria <adria@DESKTOP-T2UISSQ>
2022-03-29 23:07:11 +0200

    commit final

---
+++ generadorip/src/generadorip/GeneradorIpTest.java
@@ -0,0 +1,24 @@
+package generadorip;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class GeneradorIpTest {
+	
+	GeneradorIp generador;
+	
+	@Test
+	void testGenerarNumero() {
+		generador = new GeneradorIp();
+		
+	}
+
+	@Test
+	void test() {
+		
+	}
+
+}
